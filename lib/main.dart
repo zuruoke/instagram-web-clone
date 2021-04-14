42a6883f815a797a91cdffa2888417f7637e0243
zuruoke <zuruokeokafor2016@gmail.com>
2021-04-14 22:45:32 +0100

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/ui/tab_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(InstagramCloneApp());
 }
 
 class InstagramCloneApp extends StatefulWidget{
-
   _InstagramCloneAppState createState() => _InstagramCloneAppState();
 }
 
--- lib/ui/profile_screen.dart
+++ lib/ui/profile_screen.dart
@@ -136,20 +136,40 @@ class _ProfileScreenState extends State<ProfileScreen> {
           }
           ),
           IconButton(
-          icon: Icon(Icons.shopping_bag_rounded, size: 30,), 
-          onPressed: null
+          icon: Icon(Icons.shopping_bag_rounded, size: 30, color: selected == 'save' ? Colors.blue : null,), 
+          onPressed: (){
+            setState(() {
+              selected = 'save'; 
+            });
+          }
           ),
           IconButton(
-          icon: Icon(Icons.portrait_outlined, size: 30,), 
-          onPressed: null
+          icon: Icon(Icons.portrait_outlined, size: 30, color: selected == 'photosOfYou' ? Colors.blue : null,), 
+          onPressed: (){
+            setState(() {
+              selected = 'photosOfYou';  
+            });
+          }
           ),
         ],
       ),
       //Divider(thickness: 0.24, color: Colors.black,),
       SizedBox(height: 12,),
-      selected == 'grid' ? gridStyle() : selected == 'list' ? listStyle() : Container()
+      selected == 'grid' ? gridStyle() : 
+      selected == 'list' ? listStyle() : 
+      selected == 'save' ? saveLayout() :
+      selected == 'photosOfYou' ? photosOfYouLayout() :
+      Container()
       ],
     );
+  }
+
+  photosOfYouLayout(){
+    return Container();
+  }
+
+  saveLayout(){
+    return Container();
   }
 
   listStyle(){
